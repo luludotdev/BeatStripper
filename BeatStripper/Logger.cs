fe723fc65222eb130d8a572eb155d73abcb825de
Jack Baron <jackmbaron@gmail.com>
2019-10-24 18:48:16 +0100

    create simple logger

---
+++ BeatStripper/Logger.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BeatStripper
+{
+    internal static class Logger
+    {
+        internal static void Log(object message)
+        {
+            string time = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            Console.WriteLine($"{time} > {message.ToString()}");
+        }
+    }
+}
